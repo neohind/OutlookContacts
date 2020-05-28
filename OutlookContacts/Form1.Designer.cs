19e07a7dc86c8a59f63629460b8281cd125e12b2
김형진 <neohind@hotmail.com>
2020-05-28 12:56:45 +0900

    Add project files.

---
+++ OutlookContacts/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace OutlookContacts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(136, 56);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(148, 50);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(136, 135);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(148, 50);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 483);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 50);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(324, 56);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(664, 541);
+            this.txtResults.TabIndex = 1;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(1006, 56);
+            this.propertyGrid1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(974, 544);
+            this.propertyGrid1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2064, 623);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+    }
+}
+
---
+++ OutlookContacts/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Outlook = NetOffice.OutlookApi;
+
+namespace OutlookContacts
+{
+    public partial class Form1 : Form
+    {
+        Outlook.Application m_app = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (m_app == null)
+            {
+                m_app = new Outlook.Application();
+                Outlook._NameSpace ns = m_app.GetNamespace("MAPI");
+
+                foreach(Outlook.MAPIFolder folder in ns.Folders)
+                {
+                    txtResults.AppendText(string.Format("{0} - {1}\r\n", folder.Name, folder.DefaultItemType));
+                    TraverseFolders(folder);                    
+                }
+
+                Outlook.MAPIFolder selectedFolder  = ns.GetFolderFromID("00000000C5B7DEF515463948B280312D7005404A02870000");
+
+                foreach(object oItem in selectedFolder.Items)
+                {
+                    if (oItem is Outlook.ContactItem)
+                    {
+
+                        Outlook.ContactItem item = (Outlook.ContactItem)oItem;
+
+                        txtResults.AppendText(string.Format("{0}/{1}\r\n", item.FirstName, item.LastName));
+
+                        if (string.Equals("LastName", item.LastName))
+                        {
+                            propertyGrid1.SelectedObject = item;
+
+                            var props = TypeDescriptor.GetProperties(item);
+                            foreach (PropertyDescriptor prop in props)
+                            {
+                                try
+                                {
+                                    txtResults.AppendText(string.Format("{0} : {1}\r\n", prop.DisplayName, prop.GetValue(item)));
+                                }
+                                catch(Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                            }
+                        }                        
+                    }
+                }
+                
+            }
+        }
+
+        private void TraverseFolders(Outlook.MAPIFolder parentFolder)
+        {
+            foreach (Outlook.MAPIFolder folder in parentFolder.Folders)
+            {
+                if (folder.DefaultItemType == Outlook.Enums.OlItemType.olContactItem)
+                {
+                    txtResults.AppendText(string.Format("{0} - {1} [{2}]\r\n", folder.Name, folder.DefaultItemType, folder.EntryID));
+                    TraverseFolders(folder);
+                }
+            }
+
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(m_app != null)
+                m_app.Dispose();
+            m_app = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            Encoding cp949 = Encoding.GetEncoding(949);
+            using (StreamReader reader = File.OpenText(@"D:\MyData\Desktop\contacts2.CSV"))
+            using (FileStream stream = File.Create(@"D:\MyData\Desktop\contacts3.CSV", 1024, FileOptions.None))
+            {
+                while(reader.EndOfStream == false)
+                {
+                    string sReadLine = reader.ReadLine();
+                    byte[] aryLineData = cp949.GetBytes(sReadLine);
+                    stream.Write(aryLineData, 0, aryLineData.Length);
+                    stream.WriteByte((byte)13);
+                    stream.WriteByte((byte)10);
+                }
+                reader.Close();
+                stream.Close();
+            }
+        }
+    }
+}
