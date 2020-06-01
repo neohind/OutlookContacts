c56adf5b3351931c4df0dd6db675f88c9d08732a
김형진 <neohind@gmail.com>
2020-06-01 19:31:25 +0900

    Google API People 적용

--- OutlookContacts/Form1.Designer.cs
+++ OutlookContacts/Form1.Designer.cs
@@ -33,14 +33,16 @@
             this.button3 = new System.Windows.Forms.Button();
             this.txtResults = new System.Windows.Forms.TextBox();
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.button1 = new System.Windows.Forms.Button();
+            this.consoleWebBrowser = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(136, 56);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Location = new System.Drawing.Point(68, 29);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(148, 50);
+            this.btnStart.Size = new System.Drawing.Size(74, 26);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -48,10 +50,10 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(136, 135);
-            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStop.Location = new System.Drawing.Point(68, 70);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(148, 50);
+            this.btnStop.Size = new System.Drawing.Size(74, 26);
             this.btnStop.TabIndex = 0;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -59,10 +61,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(136, 483);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(68, 251);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 50);
+            this.button3.Size = new System.Drawing.Size(74, 26);
             this.button3.TabIndex = 0;
             this.button3.Text = "button1";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,34 +72,52 @@
             // 
             // txtResults
             // 
-            this.txtResults.Location = new System.Drawing.Point(324, 56);
-            this.txtResults.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtResults.Location = new System.Drawing.Point(162, 29);
             this.txtResults.Multiline = true;
             this.txtResults.Name = "txtResults";
             this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtResults.Size = new System.Drawing.Size(664, 541);
+            this.txtResults.Size = new System.Drawing.Size(334, 283);
             this.txtResults.TabIndex = 1;
             // 
             // propertyGrid1
             // 
             this.propertyGrid1.HelpVisible = false;
-            this.propertyGrid1.Location = new System.Drawing.Point(1006, 56);
-            this.propertyGrid1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.propertyGrid1.Location = new System.Drawing.Point(503, 29);
             this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(974, 544);
+            this.propertyGrid1.Size = new System.Drawing.Size(447, 283);
             this.propertyGrid1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // consoleWebBrowser
+            // 
+            this.consoleWebBrowser.Location = new System.Drawing.Point(162, 329);
+            this.consoleWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.consoleWebBrowser.Name = "consoleWebBrowser";
+            this.consoleWebBrowser.Size = new System.Drawing.Size(788, 250);
+            this.consoleWebBrowser.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2064, 623);
+            this.ClientSize = new System.Drawing.Size(962, 593);
+            this.Controls.Add(this.consoleWebBrowser);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.propertyGrid1);
             this.Controls.Add(this.txtResults);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -113,6 +133,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox txtResults;
         private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser consoleWebBrowser;
     }
 }
 
--- README.md
+++ README.md
@@ -5,3 +5,19 @@ Outlook의 연락처를 Gmail 안에 Import 해서 GMail 안의 연락처에 등
 
 일단 이 작업을 하려면, Export 하는 데이터의 구조를 먼저 파악해야 하므로
 이 부분을 먼저 설정하도록 한다.
+
+이 처리를 할 때, 기준이 되는 데이터가 결정될 필요가 있다.
+기준 데이터 부분이 있어 이 곳에 저장된 정보가 기준이 되고, 이 안의 값을 업데이트 한 뒤, 
+최종적으로 동기화 되게 해야 한다.
+
+기준은 Outlook으로 설정한다.
+
+Outlook -> Google Contact 로 업데이트 할 수 있게 하며,
+Google Contact 에서 Export(Outlook 스타일로)한 값을 Outlook 에 적용하게 한다.
+
+이 때, 이름이 베이스가 되고 Email 주소를 함께 Key로 사용하게 한다.
+
+상호간의 데이터 Import Export는 CSV로 할 수 있게 한다.
+
+
+TODO : Google API를 Nuget으로 받아 처리 될 수 있도록 한다.
